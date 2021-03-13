33bbe41f9cc029d47db984e0f4011767e88d4de6
Alan Martins <alanhorcrux@gmail.com>
2021-03-13 18:10:51 -0300

    finalizando desafio

--- src/index.js
+++ src/index.js
@@ -10,19 +10,69 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username == username);
+
+  if(user == null){
+    return response.status(404).json({error: 'Usuário não localizado'});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length >= 10 ){
+    return response.status(403).json({error: "Você já tem o limite maximo permitido!"});
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if(!validate(id,4)){
+    return response.status(400).json({error: 'Id fornecido não é valido!'});
+  }
+
+  const user = users.find((user) => user.username == username);
+
+  if(user == null){
+    return response.status(404).json({error: 'Usuário não localizado!'});
+  }
+
+  const todo = user.todos.find((todo) => todo.id == id);
+
+  if(todo == null){
+    return response.status(404).json({error: 'Id todo não localizado'});
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id == id);
+
+  if(user == null){
+    return response.status(404).json({error: 'Usuário não localizado!'});
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
@@ -127,4 +177,4 @@ module.exports = {
   checksCreateTodosUserAvailability,
   checksTodoExists,
   findUserById
-};
+}; 
